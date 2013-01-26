6fb7de94dbd6bc1f74fb6b7fa0cb88527e96fda0
Sebastian Barreto <Calevin@users.noreply.github.com>
2013-01-26 23:32:15 +0000

    Commit inicial de la carpeta que contiene los test del paquete navaja.mapeo

---
+++ NavajaFW/test/mapeo/MapeadorTest.java
@@ -0,0 +1,14 @@
+package mapeo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MapeadorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
