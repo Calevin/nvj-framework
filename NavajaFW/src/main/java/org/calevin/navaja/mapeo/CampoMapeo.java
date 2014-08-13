37b27ad8b6aa2736a2e3cf1ca3d46ea8c8e7db21
Sebastian Barreto <Calevin@users.noreply.github.com>
2014-08-13 09:46:30 +0000

    Se implementan los metodos hashCode, equals y compare

--- NavajaFW/src/main/java/org/calevin/navaja/mapeo/CampoMapeo.java
+++ NavajaFW/src/main/java/org/calevin/navaja/mapeo/CampoMapeo.java
@@ -1,11 +1,13 @@
 package org.calevin.navaja.mapeo;
 
+import java.util.Comparator;
+
 /**
  * Clase que representa el Campo de una tabla del mapeo
  * 
  * @author calevin
  */
-public class CampoMapeo {
+public class CampoMapeo implements Comparator<CampoMapeo>{
 
 	private String nombre;
 	
@@ -35,6 +37,45 @@ public class CampoMapeo {
 
 	public void setNombreComoAtributo(String nombreComoAtributo) {
 		this.nombreComoAtributo = nombreComoAtributo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime
+				* result
+				+ ((nombreComoAtributo == null) ? 0 : nombreComoAtributo
+						.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CampoMapeo other = (CampoMapeo) obj;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (nombreComoAtributo == null) {
+			if (other.nombreComoAtributo != null)
+				return false;
+		} else if (!nombreComoAtributo.equals(other.nombreComoAtributo))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compare(CampoMapeo o1, CampoMapeo o2) {
+	 return ((o1.getNombre()).compareTo(o2.getNombre()));
 	}
 
 }
