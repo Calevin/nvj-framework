3d640a879e562e33701cac6fdc03b936c7fec56b
Sebastian Barreto <Calevin@users.noreply.github.com>
2014-08-13 09:45:48 +0000

    Se implementan los metodos hashCode y equals

--- NavajaFW/src/main/java/org/calevin/navaja/mapeo/PrimaryKeyMapeo.java
+++ NavajaFW/src/main/java/org/calevin/navaja/mapeo/PrimaryKeyMapeo.java
@@ -19,4 +19,29 @@ public class PrimaryKeyMapeo {
 		this.campos = campos;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((campos == null) ? 0 : campos.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PrimaryKeyMapeo other = (PrimaryKeyMapeo) obj;
+		if (campos == null) {
+			if (other.campos != null)
+				return false;
+		} else if (!campos.equals(other.campos))
+			return false;
+		return true;
+	}
+
 }
