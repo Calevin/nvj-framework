7b9c7edb8ef563c59da4ee794c70d19a705bda32
Sebastian Barreto <Calevin@users.noreply.github.com>
2014-08-16 08:24:22 +0000

    Se actualiza la lista de constantes.

--- NavajaFW/src/main/java/org/calevin/navaja/util/NavajaConstantes.java
+++ NavajaFW/src/main/java/org/calevin/navaja/util/NavajaConstantes.java
@@ -2,17 +2,22 @@ package org.calevin.navaja.util;
 
 public class NavajaConstantes {
 
-	  
+    public static final String AL_INVOCAR_METODO = "al invocar metodo";	  
     public static final String CARPETA_DE_CONF = "carpeta de configuración";
     public static final String CAMPO = "campo";
     public static final String CLASE = "clase";
     public static final String COMILLA = "\"";
+    public static final String CON = "con";
+    public static final String DEFINIDA = "definida";
+    public static final String DEFINIDO  = "definido";   
     public static final String DEFINIDA_DOS_VECES = "definida dos veces";    
     public static final String DEFINIDO_DOS_VECES = "definido dos veces";
     public static final String EN = "en";
     public static final String EN_EL_MAPEO = "en el mapeo";
+    public static final String ERROR = "error";    
     public static final String ESPACIO = " ";
     public static final String EXISTE = "existe";
+    public static final String GET = "get";     
     public static final String INVALIDA = "invalida";
     public static final String INVALIDO = "invalido";
     public static final String NO = "no";
@@ -22,17 +27,21 @@ public class NavajaConstantes {
     public static final String LA = "la";
     public static final String O = "o";
     public static final String PUNTO = ".";
+    public static final String SET = "set";   
     public static final String SOPORTADA = "soportada";
     public static final String SOPORTADO = "soportado";
     public static final String TABLA = "tabla";
     public static final String VACIA = "vacia";
     public static final String VACIO = "vacio";
+    public static final String VALOR = "valor";    
     public static final String TIPO = "tipo";
     
     //COMPUESTAS
     public static final String ESPACIO_COMILLA = ESPACIO + COMILLA;
     public static final String COMILLA_ESPACIO = COMILLA + ESPACIO;
     public static final String NO_EXISTE = NO + ESPACIO + EXISTE;
+    public static final String NO_DEFINIDA = NO + DEFINIDA;
+    public static final String NO_DEFINIDO = NO + DEFINIDO;    
     public static final String NO_SOPORTADA = NO + ESPACIO + SOPORTADA;
     public static final String NO_SOPORTADO = NO + ESPACIO + SOPORTADO;
     public static final String INVALIDA_O_NO_SOPORTADA = INVALIDA + ESPACIO + O + NO_SOPORTADA;
