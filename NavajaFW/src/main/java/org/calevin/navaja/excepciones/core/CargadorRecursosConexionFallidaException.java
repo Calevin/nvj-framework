137ee1a1db129143bebd0bb0559304e870c5e4b3
Sebastian Barreto <Calevin@users.noreply.github.com>
2014-08-19 07:39:14 +0000

    Se completan los mensajes de la excepcion.

--- NavajaFW/src/main/java/org/calevin/navaja/excepciones/core/CargadorRecursosConexionFallidaException.java
+++ NavajaFW/src/main/java/org/calevin/navaja/excepciones/core/CargadorRecursosConexionFallidaException.java
@@ -2,12 +2,30 @@ package org.calevin.navaja.excepciones.core;
 
 import java.sql.SQLException;
 
+import org.calevin.navaja.util.NavajaConstantes;
+
 public class CargadorRecursosConexionFallidaException extends
 		CargadorRecursosException {
 
 	private static final long serialVersionUID = 3639964735307634346L;
+	
+	private static final String CONEXION_FALLIDA = "Conexion fallida";
 
+	public CargadorRecursosConexionFallidaException(Exception e) {
+        super(CONEXION_FALLIDA
+        		+ NavajaConstantes.PUNTO_ESPACIO
+        		+ e);
+	}
+	
 	public CargadorRecursosConexionFallidaException(SQLException e) {
-		// TODO COMPLETAR
+        super(CONEXION_FALLIDA
+        		+ NavajaConstantes.COMA_ESPACIO
+        		+ NavajaConstantes.ERROR
+        		+ NavajaConstantes.ESPACIO
+        		+ NavajaConstantes.DE
+        		+ NavajaConstantes.ESPACIO
+        		+ NavajaConstantes.TIPO_SQL 
+        		+ NavajaConstantes.PUNTO_ESPACIO
+        		+ e);
 	}
 }
