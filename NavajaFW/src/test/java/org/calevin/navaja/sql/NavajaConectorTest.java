b42ec73a34fd648ef42144a424dd3375b31bdd72
Sebastian Barreto <Calevin@users.noreply.github.com>
2015-02-20 22:07:52 +0000

    Se implementan los metodos: ejecutarQueryReturnResultSetCasoCorrectoTest y ejecutarQueryReturnIntCasoCorrectoTest.  Se utiliza el metodo static cerrarRecurso de NavajaConector.java.

--- NavajaFW/src/test/java/org/calevin/navaja/sql/NavajaConectorTest.java
+++ NavajaFW/src/test/java/org/calevin/navaja/sql/NavajaConectorTest.java
@@ -1,12 +1,13 @@
 package org.calevin.navaja.sql;
 
+import static org.junit.Assert.fail;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Properties;
 
 import javax.sql.DataSource;
@@ -15,6 +16,7 @@ import junit.framework.Assert;
 
 import org.apache.commons.dbcp.BasicDataSourceFactory;
 import org.calevin.navaja.excepciones.mapeo.MapeoException;
+import org.calevin.navaja.excepciones.sql.CerrarRecursoException;
 import org.calevin.navaja.utiltest.ConstantesParaTests;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -23,8 +25,10 @@ import org.junit.Test;
 @SuppressWarnings("deprecation")
 public class NavajaConectorTest {
 	private static String archivoMapeoProperties = ConstantesParaTests.CARPETA_ARCHIVOS_TEST + "pruebaconector/prueba_conector.properties";
-	private static String QUERY_SELECT_1 = "select database();";
+
+	private static String QUERY_SELECT_DATABASE = "select database();";
 	private static String QUERY_CREATE_TABLE = "CREATE TABLE test_create_tabla (columna int)";
+	private static String QUERY_INSERT_TEST_CREATE_TABLA = "INSERT INTO test_create_tabla VALUES (1)";
 	private static String QUERY_DROP_TABLE_TEST_CREATE = "DROP TABLE test_create_tabla;";
 	private static String DATABASE_NOMBRE = "test_database";
 	private static String COLUMNA_CONSULTA_DATABASE = "database()";
@@ -40,34 +44,34 @@ public class NavajaConectorTest {
         
         dropTablaTestCreate();
 	}
-		
+	
+	//TESTs
 	@Test
-	public void ejecutarQueryTest(){
-		ResultSet rs = NavajaConector.ejecutarQuery(QUERY_SELECT_1);
+	public void ejecutarQueryReturnResultSetCasoCorrectoTest(){
+		ResultSet rs = NavajaConector.ejecutarQuery(QUERY_SELECT_DATABASE);
 		
 		try {
 			rs.next();
 			
 			Assert.assertTrue(DATABASE_NOMBRE.equals((String)rs.getObject(COLUMNA_CONSULTA_DATABASE)));
-			
+
+			NavajaConector.cerrarRecurso(rs);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		try {
-			cerrarRecurso(rs);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			fail("Exception! " + e);
+		} catch (CerrarRecursoException e) {
+			fail("Exception! " + e);
 		}
 	}
 	
 	@Test
-	public void ejecutarQueryCreateTableTest(){
-		NavajaConector.ejecutarUpdate(QUERY_CREATE_TABLE);
+	public void ejecutarQueryReturnIntCasoCorrectoTest(){
+		int rowsModificados = NavajaConector.ejecutarUpdate(QUERY_CREATE_TABLE);
+		Assert.assertTrue(0 == rowsModificados);
+		rowsModificados = NavajaConector.ejecutarUpdate(QUERY_INSERT_TEST_CREATE_TABLA);
+		Assert.assertTrue(1 == rowsModificados);
 	}
 	
+	//METODOS UTILITARIOS:
 	private static void dropTablaTestCreate(){
         Connection con = null;
         PreparedStatement pstm = null;
@@ -75,8 +79,7 @@ public class NavajaConectorTest {
         con = proveerDataSource().getConnection();
         pstm = con.prepareStatement(QUERY_DROP_TABLE_TEST_CREATE);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Error creando la query " + e);
 		}
 
         try {
@@ -84,16 +87,14 @@ public class NavajaConectorTest {
         pstm.executeUpdate();
         System.out.println("Ejecutado ");
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Error al ejecutar la query " + e);
 		}
         
         try {
-			cerrarRecurso(con);
-			cerrarRecurso(pstm);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+        	NavajaConector.cerrarRecurso(con);
+        	NavajaConector.cerrarRecurso(pstm);
+		} catch (CerrarRecursoException e) {
+			System.out.println("Error cerrando recursos " + e);
 		}
 	}
 	
@@ -111,23 +112,5 @@ public class NavajaConectorTest {
         }
             return dataSource;
 	}
-	
-	//PARA CERRAR RECURSOS
-	private static void cerrarRecurso(Connection conn) throws SQLException {
-        if (null != conn) {
-                conn.close();
-        }
-    }
 
-	private static void cerrarRecurso(Statement stm) throws SQLException {
-        if (null != stm) {
-                stm.close();
-        }
-    }
-
-	private static void cerrarRecurso(ResultSet rs) throws SQLException {
-        if (null != rs) {
-                rs.close();
-        }
-    }
 }
