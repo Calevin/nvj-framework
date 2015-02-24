606873d74a2ef12e4929b3c1ecfaad051ef4777f
Sebastian Barreto <Calevin@users.noreply.github.com>
2015-02-24 06:15:34 +0000

    Se agrega una PK para las pruebas del metodo NavajaDao.borrarme().

--- NavajaFW/src/test/java/org/calevin/navaja/sql/mock_tabla.sql
+++ NavajaFW/src/test/java/org/calevin/navaja/sql/mock_tabla.sql
@@ -3,5 +3,6 @@ atributo_varchar varchar(10) DEFAULT NULL,
 atributo_int int(5) DEFAULT NULL,
 segundo_atributo_int int(5) DEFAULT NULL,
 tercer_atributo_int int(5) DEFAULT NULL,
-cuarto_atributo_int int(5) DEFAULT NULL
+cuarto_atributo_int int(5) DEFAULT NULL,
+CONSTRAINT pk_mock PRIMARY KEY(atributo_varchar, atributo_int)
 );
